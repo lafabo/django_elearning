be7c38ee20ce8e0efe366a56ceb4d94862de91c9
lafabo <lazyfatboy@ya.ru>
2016-07-02 02:17:37 +0500

    i came back. day-1

--- educa/courses/admin.py
+++ educa/courses/admin.py
@@ -5,18 +5,18 @@ from .models import Subject, Course, Module
 # Register your models here.
 @admin.register(Subject)
 class SubjectAdmin(admin.ModelAdmin):
-	list_display = ['title', 'slug']
-	prepopulated_fields = {'slug': ('title', )}
+    list_display = ['title', 'slug']
+    prepopulated_fields = {'slug': ('title', )}
 
 
 class ModuleInline(admin.StackedInline):
-	model = Module
+    model = Module
 
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-	list_display = ['title', 'subject', 'created']
-	list_filter = ['created', 'subject']
-	search_fields = ['title', 'overview']
-	prepopulated_fields = {'slug': ('title', )}
-	inlines = [ModuleInline]
+    list_display = ['title', 'subject', 'created']
+    list_filter = ['created', 'subject']
+    search_fields = ['title', 'overview']
+    prepopulated_fields = {'slug': ('title', )}
+    inlines = [ModuleInline]
--- educa/courses/models.py
+++ educa/courses/models.py
@@ -7,82 +7,82 @@ from .fields import OrderField
 
 # Create your models here.
 class Subject(models.Model):
-	title = models.CharField(max_length=200)
-	slug = models.SlugField(max_length=200, unique=True)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
 
-	class Meta:
-		ordering = ('title', )
+    class Meta:
+        ordering = ('title', )
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
 
 
 class Course(models.Model):
-	owner = models.ForeignKey(User, related_name='courses_created')
-	subject = models.ForeignKey(Subject, related_name='courses')
-	title = models.CharField(max_length=200)
-	slug = models.SlugField(max_length=200, unique=True)
-	overview = models.TextField()
-	created = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, related_name='courses_created')
+    subject = models.ForeignKey(Subject, related_name='courses')
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    overview = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
 
-	class Meta:
-		ordering = ('-created', )
+    class Meta:
+        ordering = ('-created', )
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
 
 
 class Module(models.Model):
-	course = models.ForeignKey(Course, related_name='modules')
-	title = models.CharField(max_length=200)
-	description = models.TextField(blank=True)
-	order = OrderField(blank=True, for_fields=['course'])
+    course = models.ForeignKey(Course, related_name='modules')
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=['course'])
 
-	class Meta:
-		ordering = ['order']
+    class Meta:
+        ordering = ['order']
 
-	def __str__(self):
-		return '{}. {}'.format(self.order, self.title)
+    def __str__(self):
+        return '{}. {}'.format(self.order, self.title)
 
 
 # Model Content for diverse content
 class Content(models.Model):
-	module = models.ForeignKey(Module, related_name='contents')
-	content_type = models.ForeignKey(ContentType, limit_choices_to={'model__in': ('text', 'video', 'image', 'file')})
-	object_id = models.PositiveIntegerField()
-	item = GenericForeignKey('content_type', 'object_id')
-	order = OrderField(blank=True, for_fields=['module'])
+    module = models.ForeignKey(Module, related_name='contents')
+    content_type = models.ForeignKey(ContentType, limit_choices_to={'model__in': ('text', 'video', 'image', 'file')})
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
+    order = OrderField(blank=True, for_fields=['module'])
 
-	class Meta:
-		ordering = ['order']
+    class Meta:
+        ordering = ['order']
 
 
 # Model for each type of content
 class ItemBase(models.Model):
-	owner = models.ForeignKey(User, related_name='%(class)s_related')
-	title = models.CharField(max_length=250)
-	created = models.DateTimeField(auto_now_add=True)
-	updated = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(User, related_name='%(class)s_related')
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
-	class Meta:
-		abstract = True
+    class Meta:
+        abstract = True
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
 
 
 # Types of content models
 class Text(ItemBase):
-	content = models.TextField()
+    content = models.TextField()
 
 
 class File(ItemBase):
-	file = models.FileField(upload_to='files')
+    file = models.FileField(upload_to='files')
 
 
 class Image(ItemBase):
-	file = models.FileField(upload_to='images')
+    file = models.FileField(upload_to='images')
 
 
 class Video(ItemBase):
-	url = models.URLField()
+    url = models.URLField()
